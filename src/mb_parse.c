556b94ae190f0101c9cc79521dddf59fd1b5717e
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-06-21 22:03:21 +0200

    add duplicate sector error

--- src/mb_parse.c
+++ src/mb_parse.c
@@ -6,11 +6,12 @@
 #include <ctype.h>
 #include <string.h>
 
-// Note: This function returns a pointer to a substring of the original string.
-// If the given string was allocated dynamically, the caller must not overwrite
-// that pointer with the returned value, since the original pointer must be
-// deallocated using the same allocator with which it was allocated.  The return
-// value must NOT be deallocated using free() etc.
+/* Note: This function returns a pointer to a substring of the original string.
+ * If the given string was allocated dynamically, the caller must not overwrite
+ * that pointer with the returned value, since the original pointer must be
+ * deallocated using the same allocator with which it was allocated.  The return
+ * value must NOT be deallocated using free() etc.
+ * */
 char *trim_whitespace(char *str) {
   char *end;
 
