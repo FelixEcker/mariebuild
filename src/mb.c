81b315d82d7f1a46ad53a3530a918e80e05b5cc9
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-06-21 22:49:34 +0200

    impl sector registering; memory leak of 28 bytes

--- src/mb.c
+++ src/mb.c
@@ -1,12 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include <mariebuild/mb_parse.h>
 #include <mariebuild/mb_execute.h>
 
 int main() {
-  struct mb_file build_file;
-  build_file.path = "./build.mb";
-  int result = parse_file(&build_file);
-  printf("result: %d\n", result);
+  struct mb_file* build_file = malloc(sizeof(mb_file));
+  build_file->path = "./build.mb";
+  int result = parse_file(build_file);
+  free(build_file->sectors);
+  free(build_file);
+  if ((result & MB_SERR_MASK_ERRNO) == MB_SERR_MASK_ERRNO)
+    result = result & 0;
+  printf("result: 0x%.8x\n", result);
   return 0;
 }
