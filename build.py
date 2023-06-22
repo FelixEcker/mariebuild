de0bc4a7f33b061e74ef79c7125f9590918bd591
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-06-22 22:27:56 +0200

    cleanup build.py some

--- build.py
+++ build.py
@@ -1,3 +1,8 @@
+# This python buildscript is intended to build mariebuild
+# if there is no build already present,
+
+# CONFIGURATION
+
 flags = "-Wall -ggdb"
 target = "mb"
 source_dir = "src/"
@@ -10,21 +15,23 @@ sources = [
             "mb.c"
           ]
 
+# BUILDING
+
 import os
 
-print("I BEG YOU TO IGNORE THAT I AM USING A PYTHON FILE TO BUILD C,")
-print("I JUST FUCKING HATE MAKEFILES AND ALL OTHER BUILD SYSTEMS,")
-print("THAT IS THE EXACT REASON WHY IM WRITING THIS!")
-
-objs = ""
-for source in sources:
-    cmd = "gcc {0} -I{1} -c -o {2} {3}".format(flags, includes, 
-                                               obj_dir+source+".o",
-                                               source_dir+source)
-    objs += obj_dir+source+".o "
+def do_cmd(cmd):
     print("=> "+cmd)
     os.system(cmd)
 
-cmd = "gcc {0} -o {1} {2}".format(flags, target, objs)
-print("=> "+cmd)
-os.system(cmd)
+def main():
+    objs = ""
+    for source in sources:
+        do_cmd("gcc {0} -I{1} -c -o {2} {3}".format(flags, includes, 
+                                                   obj_dir+source+".o",
+                                                   source_dir+source))
+        objs += obj_dir+source+".o "
+
+    do_cmd("gcc {0} -o {1} {2}".format(flags, target, objs))
+
+if __name__ == "__main__":
+    main()
