2c2bb499b17b29bb7542f5c239f6ce349bac6608
Marie <marie@ha.si>
2023-06-22 15:32:48 +0200

    add mb_log function

--- include/mariebuild/mb_utils.h
+++ include/mariebuild/mb_utils.h
@@ -1,6 +1,15 @@
 #ifndef MB_UTILS_H
 #define MB_UTILS_H
 
+#define MB_LOGLVL_LOW 0
+#define MB_LOGLVL_STD 1
+#define MB_LOGLVL_IMP 2
+#define MB_LOGLVL_SCR 3
+
+extern int mb_logging_level;
+
+void mb_log(int level, char *msg, const char *fmt, ...);
+
 int str_startswith(char *str, char *start);
 int str_endswith(char *str, char *end);
 
--- src/mb_utils.c
+++ src/mb_utils.c
@@ -1,13 +1,32 @@
 #include <mariebuild/mb_utils.h>
 
+#include <stdio.h>
 #include <string.h>
+
+int mb_logging_level = MB_LOGLVL_LOW;
+
+void mb_log(int level, char *msg, const char* fmt, ...) {
+  if (level < mb_logging_level)
+    return;
+
+  char *level_prefix;
+  switch (level) {
+    case MB_LOGLVL_LOW: level_prefix = "l>"; break;
+    case MB_LOGLVL_STD: level_prefix = ">>"; break;
+    case MB_LOGLVL_IMP: level_prefix = "!!"; break;
+    case MB_LOGLVL_SCR: level_prefix = "=>"; break;
+    default: level_prefix = "_>"; break;
+  }
+
+  printf("%s ", level_prefix);
+  printf(msg, fmt);
+}
 
 int str_startswith(char *str, char *start) {
   return 0;
 }
 
 int str_endswith(char *str, char *end) {
-  int result = 0;
   int str_len = strlen(str);
   int end_len = strlen(end);
 
