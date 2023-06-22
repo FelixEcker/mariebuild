fa935280ce971995237a789c5225727f5641c33d
Marie <marie@ha.si>
2023-06-22 15:33:03 +0200

    implement register_section and add register_field

--- src/mb_parse.c
+++ src/mb_parse.c
@@ -1,5 +1,7 @@
 #include <mariebuild/mb_parse.h>
 
+#include <mariebuild/mb_utils.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -56,27 +58,57 @@ void free_build_file(mb_file* file) {
 
 int register_sector(struct mb_file* file, char *name) {
   if (name == NULL) return MB_ERR_UNKNOWN;
-  printf("registered sector %s\n", name);
 
   // Check for duplicate sectors
-  for (int i = 0; i < file->sector_count; i++) {
-    if (strcmp(file->sectors[i].name, name) == 0) {
+  for (int i = 0; i < file->sector_count; i++)
+    if (strcmp(file->sectors[i].name, name) == 0)
       return MB_PERR_DUPLICATE_SECTOR;
-    }
-  }
 
   int wi = file->sector_count;
   file->sector_count++;
 
-  if (file->sector_count == 1) {
+  if (wi == 0) {
     file->sectors = malloc(sizeof(mb_sector));
   } else {
-    file->sectors = realloc(file->sectors, 
-                            file->sector_count * sizeof(mb_sector));
+    file->sectors = realloc(file->sectors, (wi+1) * sizeof(mb_sector));
   }
 
+  file->sectors[wi].section_count = 0;
   file->sectors[wi].name = malloc(strlen(name) + 1);
   strcpy(file->sectors[wi].name, name);
+
+  mb_log(MB_LOGLVL_LOW, "registered sector %s\n", name);
+  return MB_OK;
+}
+
+int register_section(struct mb_sector* sector, char *name) {
+  if (name == NULL) return MB_ERR_UNKNOWN;
+  
+  // Check for duplicate sections
+  for (int i = 0; i < sector->section_count; i++) 
+    if (strcmp(sector->sections[i].name, name) == 0)
+      return MB_PERR_DUPLICATE_SECTION;
+
+  int wi = sector->section_count;
+  sector->section_count++;
+
+  if (wi == 0) {
+    sector->sections = malloc(sizeof(mb_section));
+  } else {
+    sector->sections = realloc(sector->sections, (wi+1) * sizeof(mb_section));
+  }
+
+  sector->sections[wi].field_count = 0;
+  sector->sections[wi].name = malloc(strlen(name) + 1);
+  strcpy(sector->sections[wi].name, name);
+
+  mb_log(MB_LOGLVL_LOW, "registered section %s\n", name);
+  return MB_OK;
+}
+
+int register_field(struct mb_section* section, char *name, char *value) {
+  if (name == NULL || value == NULL) return MB_ERR_UNKNOWN;
+
   return MB_OK;
 }
 
@@ -95,6 +127,9 @@ int parse_line(struct mb_file* file, char *line) {
       if (result != MB_OK)
         return result; 
     } else if (strncmp(";", token, strlen(";")) == 0) break;
+    
+    if (str_endswith(token, ":") == 1)
+      register_section(&file->sectors[file->sector_count-1], token);
 
     token = strtok(NULL, delimiter);
   }
