686140bdf339da9a6ec67c386dfcb189bb091ba4
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-06-22 22:23:46 +0200

    add build stage functions

--- src/mb_execute.c
+++ src/mb_execute.c
@@ -10,11 +10,40 @@
 
 #include <mariebuild/mb_utils.h>
 
-int mb_exec_script(struct mb_file* build_file, char *name, char *lines) {
+#include <stdlib.h>
+
+/******** mb_execute.h ********/
+
+int mb_exec_script(struct mb_build* build, char *name, char *lines) {
   mb_logf(MB_LOGLVL_STD, "Executing script %s...\n", name);
   return MB_OK;
 }
 
+/******** Build Stage Functiobs ********/
+
+int mb_exec_prepare(struct mb_build* build) {
+  return MB_OK;
+}
+
+int mb_exec_prepare_mode(struct mb_build* build) {
+  return MB_OK;
+}
+
+int mb_exec_compile(struct mb_build* build) {
+  return MB_OK;
+}
+
+int mb_exec_finalize(struct mb_build* build) {
+  return MB_OK;
+}
+
 int mb_exec_build(struct mb_file build_file, struct mb_exec_params exec_params) {
+  mb_build build;
+  build.file = NULL;
+  build.stage = MB_STAGE_NONE;
+  build.build_file = build_file;
+
+  mb_exec_script(&build, "test", "test");
+
   return MB_OK;
 }
