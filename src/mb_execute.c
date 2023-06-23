f5b2b872da6e60721965c2eaf3b4d63479abff4e
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-06-23 20:50:56 +0200

    check for field files

--- src/mb_execute.c
+++ src/mb_execute.c
@@ -9,8 +9,10 @@
 #include <mariebuild/mb_execute.h>
 
 #include <mariebuild/mb_utils.h>
+#include <mariebuild/mb_parse.h>
 
 #include <stdlib.h>
+#include <string.h>
 
 /******** mb_execute.h ********/
 
@@ -30,6 +32,13 @@ int mb_exec_prepare_mode(struct mb_build* build) {
 }
 
 int mb_exec_compile(struct mb_build* build) {
+  char bfile[] = ".config/mariebuild/files";
+  mb_field *f_files = find_field(&build->build_file, 
+                                 bfile);
+
+  if ((f_files == NULL) || (f_files->value == NULL))
+    return MB_BERR_MISSING_FILES;
+
   return MB_OK;
 }
 
@@ -37,11 +46,18 @@ int mb_exec_finalize(struct mb_build* build) {
   return MB_OK;
 }
 
-int mb_exec_build(struct mb_file build_file, struct mb_exec_params exec_params) {
+int mb_exec_build(struct mb_file build_file, 
+                  struct mb_exec_params exec_params) {
   mb_build build;
   build.file = NULL;
   build.stage = MB_STAGE_NONE;
   build.build_file = build_file;
+
+  int result;
+
+  result = mb_exec_compile(&build);
+  if (result != MB_OK) 
+    return result;
 
   mb_exec_script(&build, "test", "test");
 
