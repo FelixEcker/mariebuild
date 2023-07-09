49c1e82506a3ed622242b46e1f188794fc32c92b
FelixEcker <felix0eckert0.online.de@gmail.com>
2023-07-09 17:37:46 +0200

    add missing mb_script.h file

---
+++ include/mariebuild/mb_script.h
@@ -0,0 +1,22 @@
+/*
+ * mariebuild: mb_script.h ; author: Marie Eckert
+ *
+ * Copyright (c) 2023, Marie Eckert
+ * Licensed under the BSD 3-Clause License
+ * <https://github.com/FelixEcker/mariebuild/blob/master/LICENSE>
+ */
+
+#ifndef MB_SCRIPT_H
+#define MB_SCRIPT_H
+
+#include <mariebuild/mb_types.h>
+
+typedef struct mb_script {
+  char      *name;
+  char      *lines;
+  mb_sector *parent_sector;
+} mb_script_context;
+
+int mb_exec_script(struct mb_script* script);
+
+#endif
