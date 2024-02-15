5c75ffcfee81131e63adf8381a7c3f5104998e06
FelixEcker <felix0eckert0.online.de@gmail.com>
2024-02-15 11:45:54 +0100

    pre-merge cleanup

--- setup.bash
+++ setup.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MCFG_2_COMMIT="d76bd2ac6286ce8a6c3626d7e11296a574775397"
+
 function directory_setup() {
   if [ -d lib ]; then
     rm -rf lib
@@ -30,6 +32,10 @@ function libmcfg_2_setup() {
   git clone https://github.com/FelixEcker/mcfg_2.git
 
   cd mcfg_2
+  
+  echo "==> working on commit $MCFG_2_COMMIT"
+  git checkout $MCFG_2_COMMIT 2> /dev/null
+  
   bash setup.bash || exit
   bash build.bash --lib-only || return
 
