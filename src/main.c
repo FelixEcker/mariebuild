74e4fe8ba426eaf379fe99e542f686c92f9d2b36
Marie Eckert <marie@ha.si>
2024-06-05 15:58:39 +0200

    increment version number

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@
 
 #define MARIEBUILD_COLORED_LOGO
 
-#define MARIEBUILD_VERSION "0.5.3 (develop)"
+#define MARIEBUILD_VERSION "0.5.4 (develop)"
 
 // clang-format off
 // clang-format fucks this macro definition up really badly somehow
