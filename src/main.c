f08705b9eb46e4ad39a63c940b6b5c108abca4a5
Marie Eckert <marie@ha.si>
2025-01-17 18:52:36 +0100

    set version number to 0.7.1

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@
 
 #define MARIEBUILD_COLORED_LOGO
 
-#define MARIEBUILD_VERSION "0.7.0 (develop)"
+#define MARIEBUILD_VERSION "0.7.1 (develop)"
 
 /* clang-format off */
 /* clang-format fucks this macro definition up really badly somehow */
